6fdf446091e04e8ccc8f4073ea6d3eb69c11c034
INVIiICTUS <j.enriquealorenzo@gmail.com>
2022-11-22 17:52:45 +0000

    1-6

---
+++ app.js
@@ -0,0 +1,13 @@
+// esta es mi función que suma dos números
+const sum = (a, b) => {
+    return a + b
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7, 3))
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = {
+    sum
+};
---
+++ test.js
@@ -0,0 +1,13 @@
+// importar la función sum del archivo app.js
+const {
+    sum
+} = require('./app.js');
+
+// comienza tu primera prueba
+test('adds 14 + 9 to equal 23', () => {
+    //dentro de la prueba llamamos a nuestra función sum con 2 números
+    let total = sum(14, 9);
+
+    // esperamos que la suma de esos 2 números sea 23
+    expect(total).toBe(23);
+});
