50292e0ef089db0217e24853f62af119a6d2fe95
INVIiICTUS <j.enriquealorenzo@gmail.com>
2022-11-22 18:21:06 +0000

    Ejercicio acabado

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 const fromEuroToDollar = function(valueInEuro) {
-    // convert the given valueInEuro to dollars
     let valueInDollar = valueInEuro * 1.2;
-    //return the dollar value
     return valueInDollar;
 }
 
 const fromDollarToYen = function(valueInDollar) {
-    // convert the given valueInEuro to dollars
     let valueInYen = valueInDollar * 115.60;
-    //return the dollar value
     return valueInYen;
 }
 
 const fromYenToPound = function(valueInYen) {
-    // convert the given valueInEuro to dollars
     let valueInPound = valueInYen * 0.006384;
-    //return the dollar value
     return valueInPound;
 }
 
